e44e1a55e8af2b5f8fe2791e5ad6c43abdf2ca76
Michael Corbishley <michaelcorbishley@Michaels-Air.lan>
2023-06-25 21:34:34 -0500

    added navbar scaffolding and page templates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SkillsPageComponent } from './skills-page/skills-page.component';
+import { ExperiencePageComponent } from './experience-page/experience-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'skills', component: SkillsPageComponent },
+  { path: 'experience', component: ExperiencePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ExperiencePageComponent } from './experience-page/experience-page.component';
+import { SkillsPageComponent } from './skills-page/skills-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ExperiencePageComponent,
+    SkillsPageComponent
   ],
   imports: [
     BrowserModule,
