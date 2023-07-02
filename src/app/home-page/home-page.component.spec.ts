a3857d5adb3a32da3300a4e89d05688cb23553e9
Michael Corbishley <michaelcorbishley@Michaels-Air.lan>
2023-07-02 15:14:38 -0500

    adding home page tests

--- src/app/home-page/home-page.component.spec.ts
+++ src/app/home-page/home-page.component.spec.ts
@@ -18,4 +18,35 @@ describe('HomePageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have the correct title', () => {
+    const fixture = TestBed.createComponent(HomePageComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toEqual("Welcome to my website!");
+  });
+
+  it('should render the profile img', () => {
+    const fixture = TestBed.createComponent(HomePageComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('img')).toBeTruthy;
+  });
+
+  it('should render the footer home btn', () => {
+    const fixture = TestBed.createComponent(HomePageComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('footer')).toBeTruthy;
+    expect(compiled.querySelector('footer .homeBtn')).toBeTruthy;
+  });
+
+  it('should render the github btn', () => {
+    const fixture = TestBed.createComponent(HomePageComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('footer .githubBtn')).toBeTruthy;
+  });
+
+  it('should render the linkedin btn', () => {
+    const fixture = TestBed.createComponent(HomePageComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('footer .linkedinBtn')).toBeTruthy;
+  });
 });
