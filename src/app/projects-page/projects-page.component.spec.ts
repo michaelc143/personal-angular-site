f272d65b08e2829cedbb820a9c76cc61a5758da0
Michael Corbishley <michaelcorbishley@Michaels-Air.lan>
2023-07-02 18:49:52 -0500

    updating projects page unit tests

--- src/app/projects-page/projects-page.component.spec.ts
+++ src/app/projects-page/projects-page.component.spec.ts
@@ -18,4 +18,25 @@ describe('ProjectsPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have the title', () => {
+    const fixture = TestBed.createComponent(ProjectsPageComponent);
+    expect(fixture.nativeElement.querySelector('.projects-title')).toBeTruthy;
+  });
+
+  it('should have react project', () => {
+    const fixture = TestBed.createComponent(ProjectsPageComponent);
+    component = fixture.componentInstance;
+    expect(component.reactTitle).toBeTruthy();
+    expect(component.reactTitle).toEqual("Personal Resume Site in React");
+    expect(fixture.nativeElement.querySelector('.react-proj-image')).toBeTruthy;
+  });
+
+  it('should have watch animation project', () => {
+    const fixture = TestBed.createComponent(ProjectsPageComponent);
+    component = fixture.componentInstance;
+    expect(component.watchTitle).toBeTruthy();
+    expect(component.watchTitle).toEqual("Watch Animation using OpenGl");
+    expect(fixture.nativeElement.querySelector('.watch-proj-img')).toBeTruthy;
+  });
 });
--- src/app/projects-page/projects-page.component.ts
+++ src/app/projects-page/projects-page.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class ProjectsPageComponent {
     reactTitle: String = "Personal Resume Site in React";
-    watchTitle: String = "Watch Animation using OpenGl"
+    watchTitle: String = "Watch Animation using OpenGl";
 }
