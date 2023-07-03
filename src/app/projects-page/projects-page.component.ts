afead0e89f4dbc870ffe2283a6c50716cd37498c
Michael Corbishley <mcorbishley@wisc.edu>
2023-07-02 21:17:56 -0500

    adding triangle to projects

--- src/app/projects-page/projects-page.component.ts
+++ src/app/projects-page/projects-page.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
 export class ProjectsPageComponent {
     reactTitle: String = "Personal Resume Site in React";
     watchTitle: String = "Watch Animation using OpenGl";
+    shipTitle: String = "Space Ship Animation using OpenGl";
+    triangleTitle: String = "Moveable Triangle Animation using Opengl";
     sourceCodeMsg: String = "Source Code Here!";
 }
