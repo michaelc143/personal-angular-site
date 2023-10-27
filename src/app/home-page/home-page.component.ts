8b7e74822c25c3cf345c6ff11f0eea5ffb366d2c
Michael Corbishley <mcorbishley@wisc.edu>
2023-10-27 09:29:37 -0500

    minor fixups, change theme to purple

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -8,6 +8,6 @@ import { Component } from '@angular/core';
 export class HomePageComponent {
   welcomeHeader: String = "Welcome to my website!";
   toolbarTip: String = "Use the toolbar above to navigate to other pages";
-  para1: String = "Hello, I'm Michael! I'm a web developer and computer science student at UW Madison, specializing in Angular and Java. I possess a strong foundation in software development principles and methodologies, constantly expanding my knowledge through academic pursuits and practical as well as professional experience."
-  para2: String = "Committed to delivering high-quality solutions, I stay up to date with the latest industry trends and technologies. Whether it's starting from scratch or enhancing existing projects, I'm eager to collaborate on challenging web development endeavors. Let's connect and bring your ideas to life using Angular and Java!";
+  para1: String = "Hello, I'm Michael! I'm a web developer and 4th-year computer science student at UW Madison, specializing in Angular, React, Python and Java."
+  para2: String = "Dedicated to providing top-notch solutions, I keep myself well-informed about the latest industry trends and cutting-edge technologies. Whether it involves building projects from the ground up or improving existing ones, I'm enthusiastic about teaming up for demanding web development challenges.";
 }
