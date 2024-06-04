b4a432ddc999233d9e84364b787d5f1ad5775c41
Michael Corbishley <mcorbishley@wisc.edu>
2024-06-04 13:17:34 -0500

    update degree and webdev UW to relevant info

--- src/app/experience-page/experience-page.component.ts
+++ src/app/experience-page/experience-page.component.ts
@@ -15,13 +15,13 @@ export class ExperiencePageComponent {
   "Enhanced my professional development by actively participating in weekly hour-long technology seminars covering a diverse range of topics such as microservice architecture, machine learning, and cloud deployment.",
   "Collaborated with business representatives in bi-weekly meetings to showcase ongoing progress towards quarterly objectives and deliver captivating demonstrations of new features to stakeholders."
   ]
-  webdevUW = "WebDev UW (August 2020 - Current)"
+  webdevUW = "WebDev UW (August 2020 - May 2024)"
   webdevUWTopics = ["UW-Madison student organization dedicated to advancing web development skills",
   "WebHacks 2021: Created a bingo game using React to teach computer science to users"
   ]
-  degree = "Computer Science Student at the University of Wisconsin - Madison (August 2020 - Current)"
-  degreeTopic = "Actively pursuing a Bachelor's of Science degree in Computer Science, delving into the realms of knowledge to advance expertise in cutting-edge technologies and problem-solving methodologies."
-  degreeCourses = "Computer Engineering & Memory Management, Introduction To Algorithms, Linear Algebra, Discrete Math, Artificial Intelligence, Computer Graphics, Java Programming (I - III), Operating Systems, Cryptography"
+  degree = "Computer Science Student at the University of Wisconsin - Madison (August 2020 - May 2024)"
+  degreeTopic = "Earned a Bachelor's of Science degree in Computer Science, delving into the realms of knowledge to advance expertise in cutting-edge technologies and problem-solving methodologies."
+  degreeCourses = "Computer Engineering & Memory Management, Introduction To Algorithms, Linear Algebra, Discrete Math, Artificial Intelligence, Computer Graphics, Java Programming (I - III), Operating Systems, Cryptography, Big Data Systems, UI/UX Development, VR, Software Engineering."
   degreeCourseTitle = "Major Related Course List"
   teachingAssistant = "AP JAVA Teaching Assistant (August 2019 - January 2020)"
   teachingAssistantTopics = ["Played an instrumental role in instructing and mentoring high school students in AP Java, fostering their understanding and mastery of advanced Java concepts.",
