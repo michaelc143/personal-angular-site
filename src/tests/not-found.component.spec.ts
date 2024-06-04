db5731dd5aeda2de5966d8630e5031d8a26be817
Michael Corbishley <mcorbishley@wisc.edu>
2024-06-04 14:13:06 -0500

    add router testing for full coverage

--- src/tests/not-found.component.spec.ts
+++ src/tests/not-found.component.spec.ts
@@ -1,20 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 import { NotFoundComponent } from 'src/app/not-found/not-found.component';
 
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent;
   let fixture: ComponentFixture<NotFoundComponent>;
+  let router: Router;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [NotFoundComponent],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [NotFoundComponent]
-    });
     fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to home when navigateToHome is called', () => {
+    spyOn(router, 'navigate');
+    component.navigateToHome();
+    expect(router.navigate).toHaveBeenCalledWith(['/']);
   });
 });
