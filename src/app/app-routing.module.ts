1b3284e94d9d28246a2e1aaa0fd0bf31409203aa
Michael Corbishley <mcorbishley@wisc.edu>
2024-06-10 15:26:43 -0500

    sort imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
 import { ExperiencePageComponent } from './experience-page/experience-page.component';
-import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
